7faf69e4e3edc67dbe1d79bff54f04a7bc4a10dd
fatmaasal <fasal@etu.edu.tr>
2016-10-12 14:20:03 +0300

    Demo app committed

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,15 @@
 def mulsearch(array,element1,element2):
- if array == None:
+ if array is None:
   return False
- for e in array:
-  if(e*element1)==element2:
-   return True
- return False
+ elif element1 is None: 
+  return False 
+ elif element2 is None:
+  return False
+ else: 
+  for e in array:
+   e = int(e)
+   element2 = int(element2)
+   mul = e * int(element1)
+   if mul == element2 :
+    return True
+  return False
